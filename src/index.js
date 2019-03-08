2be587074fcc217aec4ea880df2ccc1594d90577
Hoang Nhat Thuan <meosieudang>
2019-03-08 08:35:54 +0700

    close mode dev

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const composeEnhancers =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    composeEnhancers
+    // composeEnhancers
   )
 );
 
